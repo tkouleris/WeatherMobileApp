2daca9362a46052d87a9cdebbb68f63cde5cbc38
Thodoris Kouleris <kurokokoro@windowslive.com>
2024-01-12 07:11:50 +0200

    refactor: HourForecast component

--- components/ForecastDay.js
+++ components/ForecastDay.js
@@ -1,4 +1,7 @@
 import {Image, StyleSheet, Text, View} from "react-native";
+import {Ionicons} from '@expo/vector-icons'
+import HourForecast from "./HourForecast";
+
 
 function ForecastDay({item}){
     let forecast_day = item.item[0].day;
@@ -12,20 +15,11 @@ function ForecastDay({item}){
             </View>
             <View >
                 {item.item.map((hourData, key) => {
+
                     // console.log('https://openweathermap.org/img/wn/'+hourData.icon+'@2x.png')
                     return (
-                        <View style={styles.forecast_day_hour_container} key={key}>
-                            <Text style={styles.forecast_day_hour_text}>{hourData.time}</Text>
-                            <Text style={styles.forecast_day_hour_text}> {hourData.temp} °C</Text>
-                            <Text style={styles.forecast_day_hour_text}> {hourData.humidity} %</Text>
-                            <Text style={styles.forecast_day_hour_text}> {hourData.wind_speed} m/s</Text>
-                            <Text style={styles.forecast_day_hour_text}> {hourData.description} </Text>
-                            {/*<Image style={styles.forecast_day_hour_text}*/}
-                            {/*    source={{*/}
-                            {/*        uri: 'https://openweathermap.org/img/wn/'+hourData.icon+'@2x.png',*/}
-                            {/*    }}*/}
-                            {/*/>*/}
-                        </View>
+                        <HourForecast key={key} itemKey={key} hourData={hourData} />
+
 
                     );
                 })}
@@ -39,10 +33,11 @@ export default ForecastDay;
 const styles = StyleSheet.create({
     forecast_day_container:{
         paddingHorizontal: 5,
+        paddingBottom:5,
         borderRadius: 8,
         elevation: 18,
         marginBottom:30,
-        height: 200,
+        height: 'auto',
         backgroundColor: '#D3D3D3',
         width: '100%',
         justifyContent: 'flex-start',
@@ -60,11 +55,4 @@ const styles = StyleSheet.create({
     forecast_day_container_title:{
         fontWeight: 'bold'
     },
-    forecast_day_hour_container:{
-        flexDirection:'row'
-    },
-    forecast_day_hour_text:{
-        marginRight: 5
-    }
-
 })
---
+++ components/HourForecast.js
@@ -0,0 +1,57 @@
+import {StyleSheet, Text, View} from "react-native";
+import {Ionicons} from "@expo/vector-icons";
+
+function HourForecast({itemKey, hourData}){
+    return <View style={styles.forecast_day_hour_container} key={itemKey}>
+        <View style={styles.forecast_day_hour_container_data}>
+            <View style={styles.forecast_day_hour_container_datum}>
+                <Ionicons name={'time'} size={18} />
+                <Text style={styles.forecast_day_hour_text}>{hourData.time}</Text>
+            </View>
+            <View style={styles.forecast_day_hour_container_datum}>
+                <Ionicons name={'thermometer'} size={18} />
+                <Text style={styles.forecast_day_hour_text}> {hourData.temp} °C</Text>
+            </View>
+            <View style={styles.forecast_day_hour_container_datum}>
+                <Ionicons name={'water'} size={18} />
+                <Text style={styles.forecast_day_hour_text}> {hourData.humidity} %</Text>
+            </View>
+            <View style={styles.forecast_day_hour_container_datum}>
+                <Ionicons name={'flag'} size={18} />
+                <Text style={styles.forecast_day_hour_text}> {hourData.wind_speed} m/s</Text>
+            </View>
+        </View>
+        <View style={styles.forecast_day_hour_container_description}>
+            <Text style={styles.forecast_day_hour_text}> {hourData.description} </Text>
+        </View>
+
+    </View>
+}
+
+export default HourForecast;
+
+const styles = StyleSheet.create({
+    forecast_day_hour_container:{
+        marginTop: 5,
+        flexDirection:'column',
+        borderBottomWidth: 1
+    },
+    forecast_day_hour_container_data:{
+        width: '100%',
+        flexDirection:'row',
+        alignItems:'center',
+    },
+    forecast_day_hour_container_datum:{
+        width: '25%',
+        flexDirection:'row',
+        alignItems:'center',
+    },
+    forecast_day_hour_text:{
+        marginRight: 5
+    },
+    forecast_day_hour_container_description:{
+        width: '100%',
+        alignItems:'flex-start',
+        justifyContent:'flex-start'
+    },
+})
